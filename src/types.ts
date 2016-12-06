44bf52199979c141f73c1e4ba8e46ff40fdc4359
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-12-06 12:25:48 -0700

    c# type parser

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,24 @@
+enum CsTypeCategory {
+    /**A type that can be represented as a collection of items */
+    Enumerable,
+    /**A dictionary is equivalent to a typescript object */
+    Dictionary,
+    /**A c$# nullable type where the value type is the first generic type */
+    Nullable,
+    /**A c# tuple */
+    Tuple,
+    /**A boolean type */
+    Boolean,
+    /**A numeric type */
+    Number,
+    /**A date type */
+    Date,
+    /**Any type */
+    Any,
+    /**Unidentified type */
+    Other
+}
+
 /**A c# type */
 class CsType {
     constructor(name: string, generics: CsType[], array: CsArray[]) {
@@ -6,6 +27,9 @@ class CsType {
         this.array = array;
     }
 
+    /**Nullable type name */
+    static nullable = "Nullable";
+
     /**Type name */
     name: string;
     /**Generic arguments */
@@ -13,8 +37,118 @@ class CsType {
 
     /**Neasted arrays */
     array: CsArray[];
-}
 
+    /**Gets the type category */
+    get category(): CsTypeCategory {
+        var enumerables = ["List", "ObservableCollection", "Array", "IEnumerable", "IList", "IReadOnlyList", "Collection", "ICollection"];
+        var dictionaries = ["Dictionary", "IDictionary"];
+
+        var bools = ["bool", "Boolean", "System.Boolean"];
+        var numbers = [
+            'int', "Int32", "System.Int32",
+            'float', "Single", "System.Single",
+            'decimal', "Decimal", "System.Decimal",
+            'long', "Int64", "System.Int64",
+            'byte', "Byte", "System.Byte",
+            'sbyte', "SByte", "System.SByte",
+            'short', "Int16", "System.Int16",
+            'ushort', "UInt16", "System.UInt16",
+            'ulong', "UInt64", "System.UInt64"
+        ];
+        var dates = ["DateTime", "System.DateTime", "DateTimeOffset", "System.DateTimeOffset"];
+        var anys = ["object", "System.Object", "dynamic"];
+        if (enumerables.indexOf(this.name) != -1 && this.generics.length <= 1) {
+            return CsTypeCategory.Enumerable;
+        } else if (dictionaries.indexOf(this.name) != -1 && this.generics.length == 2) {
+            return CsTypeCategory.Dictionary;
+        } else if (this.name == CsType.nullable && this.generics.length == 1) {
+            return CsTypeCategory.Nullable;
+        } else if (this.name == "Tuple" && this.generics.length > 0) {
+            return CsTypeCategory.Tuple;
+        } else if (bools.indexOf(this.name) != -1 && this.generics.length == 0) {
+            return CsTypeCategory.Boolean;
+        } else if (numbers.indexOf(this.name) != -1 && this.generics.length == 0) {
+            return CsTypeCategory.Number;
+        } else if (dates.indexOf(this.name) != -1 && this.generics.length == 0) {
+            return CsTypeCategory.Date;
+        } else if (anys.indexOf(this.name) != -1 && this.generics.length == 0) {
+            return CsTypeCategory.Any;
+        } else {
+            return CsTypeCategory.Other;
+        }
+    }
+
+    /**Convert this type to a typescript type */
+    private convertToTypescriptNoArray(): string {
+        switch (this.category) {
+            case CsTypeCategory.Enumerable: {
+                if (this.generics.length == 0) {
+                    return "any[]";
+                } else if (this.generics.length == 1) {
+                    return this.generics[0].convertToTypescript() + "[]";
+                } else {
+                    throw "";
+                }
+            }
+
+            case CsTypeCategory.Dictionary: {
+                if (this.generics.length == 2) {
+                    return `{ [key: ${this.generics[0].convertToTypescript()} ] : ${this.generics[1].convertToTypescript()} }`;
+                } else {
+                    throw "";
+                }
+            }
+            case CsTypeCategory.Nullable: {
+                if (this.generics.length == 1) {
+                    return `(${this.generics[0].convertToTypescript()} | null)`;
+                }
+                else {
+                    throw "";
+                }
+            }
+            case CsTypeCategory.Tuple: {
+                if (this.generics.length == 0)
+                    throw "";
+                let x: { Item1: number, Item2: boolean };
+                let tupleElements = this.generics.map((v, i) => `Item${i + 1}: ${v.convertToTypescript()}`);
+                let join = tupleElements.reduce((a, b) => a ? a + ", " + b : b, "");
+                return `{ ${join} }`;
+            }
+            case CsTypeCategory.Boolean: {
+                return "boolean";
+            }
+            case CsTypeCategory.Number: {
+                return "number";
+            }
+            case CsTypeCategory.Date: {
+                return "Date";
+            }
+            case CsTypeCategory.Any: {
+                return "Any";
+            }
+            case CsTypeCategory.Other: {
+                if (this.generics.length > 0) {
+                    var generics = this.generics.map(x => x.convertToTypescript()).reduce((a, b) => a ? a + ", " + b : b, "");
+                    return `${this.name}<${generics}>`;
+                } else {
+                    return this.name;
+                }
+            }
+        }
+    }
+
+    private convertToTypescript(): string {
+        var arrayStr = "";
+        for (var a of this.array) {
+            arrayStr += "[";
+            for (var i = 1; i < a.dimensions; i++) {
+                arrayStr += ",";
+            }
+            arrayStr += "]";
+        }
+        return this.convertToTypescript() + arrayStr;
+    }
+}
 /**A c# array */
 interface CsArray {
     /**Array dimensions */
@@ -73,23 +207,32 @@ function parseArray(code: string): CsArray[] {
 }
 
 /**Parse a C# type, returns null if the given type could not be parsed */
-function parseType(code: string): CsType | null {
+export function parseType(code: string): CsType | null {
     //Remove all spaces:
     code = code.replace(" ", "");
 
-    //group 1: Type name
-    //group 2: Generic arguments
-    //group 3: Arrays
-    var patt = /([a-zA-Z0-9_]+)\s*(?:<(.*)>)?\s*(\[[,\[\]]*\])*/;
+
+    var patt = /([a-zA-Z0-9_]+)\s*(?:<(.*)>)?\s*(\?)?\s*(\[[,\[\]]*\])*/;
 
     var arr = patt.exec(code);
     if (!arr) {
         return null;
     }
-    var name = arr[1];
-    var generics = splitCommas(arr[2] || "");
-    var arrays = arr[3] || "";
 
-    return new CsType(name, generics.map(x => parseType(x)), parseArray(arrays));
+    //Pattern groups:
+    var name = arr[1];
+    var genericsStr = splitCommas(arr[2] || "");
+    var nullable = arr[3] == "?";
+    var arraysStr = arr[4] || "";
+
+    var arrays = parseArray(arraysStr);
+    var generics = genericsStr.map(x => parseType(x));
+    if (nullable) {
+        var underlyingType = new CsType(name, generics, []);
+        return new CsType(CsType.nullable, [underlyingType], arrays);
+    } else {
+        return new CsType(name, generics, arrays);
+    }
 }
+
 
